1171fab4b3a296ff8b426e2b04d8bbd8444c37e2
Samuel J. Taylor <samtaylor.uk@gmail.com>
2016-10-25 02:40:00 +0100

    fieds

--- 10-linkconfig.sh
+++ 10-linkconfig.sh
@@ -24,16 +24,16 @@ else
   chown www-data:www-data /config/Configure.php
 fi
 
-if [ ! -e '/data/covers' ]; then
+if [ ! -d '/data/covers' ]; then
     echo "  ** Copying default covers dir"
     cp -R /app/nzedb/resources/covers-sample /data/covers
     chown www-data:www-data /data/covers
     chmod -R 777 /data/covers
 fi
 
-if [ ! -e '/app/nzedb/nzedb/config' ]; then
+if [ ! -d '/app/nzedb/nzedb/config' ]; then
     echo "  ** Removing old config dir link"
-    rm /app/nzedb/nzedb/config
+    rm -R /app/nzedb/nzedb/config
 fi
 
 echo "  ** linking conf dir"
@@ -41,9 +41,9 @@ ln -s /config /app/nzedb/nzedb/config
 chown www-data:www-data /app/nzedb/nzedb/config
 
 
-if [ ! -e '/app/nzedb/resources/covers' ]; then
+if [ ! -d '/app/nzedb/resources/covers' ]; then
     echo "  ** Removing old config dir link"
-    rm /app/nzedb/resources/covers
+    rm -R /app/nzedb/resources/covers
 fi
 
 echo "  ** linking conf dir"
